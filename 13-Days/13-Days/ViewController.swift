8cad14f22f4ed94527c2a2eeedca83445f0fed2f
Hakan ÖZER <hakanozer02@gmail.com>
2021-02-11 21:11:47 +0300

    13-14

---
+++ 13-Days/13-Days/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  13-Days
+//
+//  Created by HAKAN ÖZER on 30.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ 14-Days/14-Days/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  14-Days
+//
+//  Created by HAKAN ÖZER on 2.02.2021.
+//
+
+import UIKit
+import ProgressHUD
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        ProgressHUD.showSucceed()
+        
+    }
+
+
+}
+
